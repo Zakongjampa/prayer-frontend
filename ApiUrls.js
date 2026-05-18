07eadcd113216311ee938abe7d212053713dca42
Zakongjampa <Zakongjampa@gmail.com>
2026-05-18 22:27:10 +0200

    API url update

--- ApiUrls.js
+++ ApiUrls.js
@@ -1,7 +1,12 @@
-const BASE_URL = meta.env.VITE_API_URL | "http://localhost:8080";
+const BASE_URL = getBaseUrl();
 
 // ---- Users -------
 export const API_URL_USERS =  `${BASE_URL}/user`;
 export const API_URL_PRAYERS =  `${BASE_URL}/prayer`;
 export const API_URL_LIKES =  `${BASE_URL}/liked`;
 export const API_URL_LOGIN = `${BASE_URL}/login`;
+
+function getBaseUrl() {
+    // Fallback for when the env var isn’t defined (e.g. local dev)
+    return import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
+  }
